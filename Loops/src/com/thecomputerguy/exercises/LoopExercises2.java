974705ad57cff8208088a7c0d09082ee4a82913f
Super-Yojan <gautamyojan0@gmail.com>
2019-11-22 22:41:34 -0500

    Loops and String Exercises

---
+++ Loops/src/com/thecomputerguy/exercises/LoopExercises2.java
@@ -0,0 +1,42 @@
+package com.thecomputerguy.exercises;
+
+public class LoopExercises2 {
+
+	/**
+	 * returns the number reversed. Do not convert to a String! remember that n*10
+	 * move the decimal place to the right 1 so 87*10 = 870 reverseNum(12345) -->
+	 * 54321 reverseNum(1) --> 1 reverseNum(0) --> 0 reverseNum(-10) --> -1
+	 */
+
+	public static int reverseNum(int num) {
+		return 0;
+	}
+
+	/**
+	 * returns the length of the number entered this method will be used by
+	 * countNumbers length(10) --> 2 length(0) --> 1 length(161) --> 3
+	 */
+	public static int length(int num) {
+		return 0;
+	}
+
+	/**
+	 * countNumbers returns how many time a given number appears within another
+	 * number. Do not convert to a String. You will need to use the length method
+	 * defined above. countNumbers(12234, 2) --> 2 countNumbers(3343032, 3) --> 4
+	 * countNumbers(3456342234, 34) --> 3 countNumbers(12345, 12345) --> 1
+	 */
+	public static int countNumbers(int num, int pattern) {
+		return 0;
+	}
+
+	/**
+	 * returns the number of words in the String given. Please note that words are
+	 * separated by a space. However, if someone typos and adds more than 1 space,
+	 * this must be considered. countWords("I love CS") --> 3 countWords(" I love
+	 * CS") --> 3 countWords("IloveCS") --> 1 countWords("") --> 0
+	 */
+	public static int countWords(String phrase) {
+		return 0;
+	}
+}
---
+++ Loops/src/com/thecomputerguy/exercises/solution/LoopExercises2Test.java
@@ -0,0 +1,68 @@
+package com.thecomputerguy.exercises.solution;
+
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class LoopExercises2Test {
+
+   @Test
+   public void reverseNumTest(){
+      int result = LoopExercises2.reverseNum(12345);
+      assertEquals(54321, result);
+      result = LoopExercises2.reverseNum(1000);
+      assertEquals(1, result);
+      result = LoopExercises2.reverseNum(0);
+      assertEquals(0, result);
+      result = LoopExercises2.reverseNum(1100003);
+      assertEquals(3000011, result);
+   }
+   
+   @Test
+   public void lengthTest(){
+      int result = LoopExercises2.length(12345);
+      assertEquals(5, result);
+      result = LoopExercises2.length(10);
+      assertEquals(2, result);
+      result = LoopExercises2.length(0);
+      assertEquals(1, result);
+   }
+   
+   @Test
+   public void countNumbersTest(){
+      int result = LoopExercises2.countNumbers(11022011, 1);
+      assertEquals(4, result);
+      result = LoopExercises2.countNumbers(11022011, 11);
+      assertEquals(2, result);
+      result = LoopExercises2.countNumbers(343434304, 34);
+      assertEquals(3, result);
+      result = LoopExercises2.countNumbers(123456, 89);
+      assertEquals(0, result);
+      result = LoopExercises2.countNumbers(0,0);
+      assertEquals(1,result);
+      result = LoopExercises2.countNumbers(5678, 5678);
+      assertEquals(1,result);
+   }
+   
+   @Test
+   public void countWordsTest(){
+      int result = LoopExercises2.countWords("I love CS");
+      assertEquals(3,result);
+      result = LoopExercises2.countWords("   I love CS");
+      assertEquals(3, result);
+      result = LoopExercises2.countWords("I     love       CS");
+      assertEquals(3, result);
+      result = LoopExercises2.countWords("IloveCS");
+      assertEquals(1, result);
+      result = LoopExercises2.countWords("");
+      assertEquals(0,result);
+      result = LoopExercises2.countWords("      ");
+      assertEquals(0,result);
+      result = LoopExercises2.countWords("This   is a really long sentence with     varying amounts of space in it   ");
+      assertEquals(13, result);
+      
+   }
+
+}
